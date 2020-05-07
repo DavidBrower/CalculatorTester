b0e1d50ed7953e8ba2d517030f7f924705ac7134
David Brower <david.brower@visionware.com>
2020-05-07 22:34:14 +0100

    Add project files.

---
+++ CalculatorTester/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorTester
+{
+    public class Calculator
+    {
+        public double Add(double d, double d1)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CalculatorTester/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTester
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void CanAddTwoNumbers()
+        {
+            var calculator = new Calculator();
+            calculator.Add(2.3, 5.6).Should().Be(7.9);
+        }
+    }
+}
